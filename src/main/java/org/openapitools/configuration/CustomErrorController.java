88d6aeb3e606f822db524c3f75c83a71c4a69d36
S7uX <valerio.cocco@outlook.de>
2022-11-22 16:56:53 +0100

    custom error controller

---
+++ src/main/java/org/openapitools/configuration/CustomErrorController.java
@@ -0,0 +1,24 @@
+//package org.openapitools.configuration;
+//
+//import org.springframework.boot.web.servlet.error.ErrorController;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//@Controller
+//public class CustomErrorController implements ErrorController {
+//
+//
+//    @RequestMapping("/error")
+//    @ResponseBody
+//    String error(HttpServletRequest request) {
+//        return "<h1>Error</h1>";
+//    }
+//
+////    @Override
+////    public String getErrorPath() {
+////        return "/error";
+////    }
+//}
