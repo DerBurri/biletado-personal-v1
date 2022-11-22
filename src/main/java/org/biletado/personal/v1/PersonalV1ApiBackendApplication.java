e02cf89e0eea0730c80c9510f692d7a4acba8aed
mburr <maximilian.burr@outlook.de>
2022-11-22 19:50:18 +0100

    refactoring renamed package

--- src/main/java/org/biletado/personal/v1/PersonalV1ApiBackendApplication.java
+++ src/main/java/org/biletado/personal/v1/PersonalV1ApiBackendApplication.java
@@ -8,11 +8,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.openapitools", "org.openapitools.api" , "org.openapitools.configuration"})
-public class OpenApiGeneratorApplication {
+@ComponentScan(basePackages = {"org.biletado.personal.v1", "org.biletado.personal.v1.api" , "org.biletado.personal.v1.configuration"})
+public class PersonalV1ApiBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OpenApiGeneratorApplication.class, args);
+        SpringApplication.run(PersonalV1ApiBackendApplication.class, args);
     }
 
     @Bean
