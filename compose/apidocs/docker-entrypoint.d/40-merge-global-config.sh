199a485dd9b29b1fdd5aeb350b273fc80425fe4c
S7uX <valerio.cocco@outlook.de>
2022-11-22 11:22:44 +0100

    delete compose dir

--- compose/apidocs/docker-entrypoint.d/40-merge-global-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-which yq >/dev/null || apk --no-cache add yq
-
-for file in /usr/share/nginx/html/openapi/*.yaml; do
-  yq -i eval-all 'select(filename == "/tmp/global-config.yaml") * select(fileIndex == 0)' ${file} /tmp/global-config.yaml
-done
