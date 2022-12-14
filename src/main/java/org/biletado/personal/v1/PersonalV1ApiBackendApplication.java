3588e0a299956df03e9c137b601349c547205daa
Maximilian Burr <maximilian.burr@outlook.de>
2022-12-14 16:36:47 +0100

    read config env from application.properties

--- src/main/java/org/biletado/personal/v1/PersonalV1ApiBackendApplication.java
+++ src/main/java/org/biletado/personal/v1/PersonalV1ApiBackendApplication.java
@@ -7,27 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.util.HashMap;
-
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.biletado.personal.v1", "org.biletado.personal.v1.api", "org.biletado.personal.v1.configuration"})
 public class PersonalV1ApiBackendApplication {
-    public static String[] EnvironmentVariableNames = new String[]{
-            "POSTGRES_PERSONAL_HOST", "POSTGRES_PERSONAL_PORT", "POSTGRES_PERSONAL_DBNAME",
-            "POSTGRES_PERSONAL_USER", "POSTGRES_PERSONAL_PASSWORD"
-    };
-
-    public static HashMap<String, String> ConfigVariables = new HashMap<>();
-
 
     public static void main(String[] args) {
-        System.out.println("READING ENVIRONMENT VARIABLES");
-        for (String environmentVariableName : EnvironmentVariableNames) {
-            String val = System.getenv(environmentVariableName);
-            ConfigVariables.put(environmentVariableName, val);
-            System.out.println(environmentVariableName + ": " + val);
-        }
-
         System.out.println("\n\n STARTING SPRING BOOT SERVER");
         SpringApplication.run(PersonalV1ApiBackendApplication.class, args);
     }
