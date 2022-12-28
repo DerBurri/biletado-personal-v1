ae61dd6e6f95bf15a427fa23cc1039fe967452dd
Maximilian Burr <maximilian.burr@outlook.de>
2022-12-28 16:37:31 +0100

    Added Repositories for JPA Entities and Modified OpenAPI DTOs for Entity Storage. Also Added Necessary Dopondoncies to pom.xml to Support Spring Data Repositories. Changed application.properties to use ENVIRONMENT_VARIABLES for GETTING Information for DB Connection to PostGres. Changed Dockerfile to use Cache for Building. Speeds up the Builds like they are on Speed. Brummmmmmmmmmmmmmmmmmmmmmmmm

--- src/main/java/org/biletado/personal/v1/api/ApiUtil.java
+++ src/main/java/org/biletado/personal/v1/api/ApiUtil.java
@@ -1,7 +1,10 @@
 package org.biletado.personal.v1.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.context.request.NativeWebRequest;
 
+import javax.persistence.Entity;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -12,6 +15,20 @@ public class ApiUtil {
             res.setCharacterEncoding("UTF-8");
             res.addHeader("Content-Type", contentType);
             res.getWriter().print(example);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void setResponseFromModel(NativeWebRequest req, String contentType, Object entity) throws JsonProcessingException
+    {
+        try {
+            HttpServletResponse res = req.getNativeResponse(HttpServletResponse.class);
+            res.setCharacterEncoding("UTF-8");
+            res.addHeader("Content-Type", contentType);
+            ObjectMapper mapper = new ObjectMapper();
+            String response = mapper.writeValueAsString(entity);
+            res.getWriter().print(response);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/biletado/personal/v1/api/PersonalApiController.java
+++ src/main/java/org/biletado/personal/v1/api/PersonalApiController.java
@@ -1,19 +1,23 @@
 package org.biletado.personal.v1.api;
 
 
-import com.sun.tools.javac.util.DefinedBy;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.biletado.personal.v1.model.Employee;
 import org.biletado.personal.v1.model.PersonalEmployeesGet200Response;
 import org.biletado.personal.v1.model.PersonalStatusGet200Response;
 import org.biletado.personal.v1.repository.AssignmentRepository;
 import org.biletado.personal.v1.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
 
 import java.util.Optional;
+import java.util.UUID;
 import javax.annotation.Generated;
 
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-11-22T12:08:12.414629Z[Etc/UTC]")
@@ -48,6 +52,50 @@ public class PersonalApiController implements PersonalApi {
     public ResponseEntity<PersonalEmployeesGet200Response> personalEmployeesGet()
     {
         Iterable<Employee> employeeIterable = employees.findAll();
-        return PersonalApi.super.personalEmployeesGet();
+        getRequest().ifPresent(request ->
+        {
+            for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    ObjectMapper mapper = new ObjectMapper();
+
+                    try {
+
+                        ApiUtil.setResponseFromModel(request, "application/json", employeeIterable);
+
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                }
+            }
+        });
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Employee> personalEmployeesIdGet(UUID id) {
+
+        Employee employeeOptional = employees.findById(id).orElse(null);
+
+        if (employeeOptional == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        getRequest().ifPresent(request ->
+        {
+            for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    ObjectMapper mapper = new ObjectMapper();
+
+                    try {
+
+                        ApiUtil.setResponseFromModel(request, "application/json", employeeOptional);
+
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                }
+            }
+        });
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
